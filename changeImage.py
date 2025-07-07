a8e4440387a7f3e717bcac654d3c6b36fbc21aed
syeedf <syeed.farrakhan@bison.howard.edu>
2025-07-07 17:24:50 -0400

    Added the change image script that changes the size and type of file and added the given example script to post images(still needs to be updated for use

---
+++ changeImage.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+from PIL import Image
+import os
+
+image_location= os.path.expanduser("~/supplier-data/images/")
+
+for filename in os.listdir(image_location):
+    if filename[0] != '.':
+        filepath= os.path.join(image_location,filename)
+        image= Image.open(filepath)
+        newimage= image.resize((600,400)).convert('RGB')
+        name, ext = os.path.splitext(filename)
+        new_filename = f"{name}.jpeg"
+        new_path = os.path.join(image_location, new_filename)
+        newimage.save(new_path)
